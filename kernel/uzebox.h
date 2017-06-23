7ea0e2f6539b21265faa233cbb851e158dbb9bed
Lee Weber <weber21w@gmail.com>
2017-06-22 22:27:39 -0500

    Update uzebox.h

--- kernel/uzebox.h
+++ kernel/uzebox.h
@@ -103,10 +103,10 @@
 		extern u8 SongBufBytes();
 		extern u8 SongBufFull();
 		#ifndef SONG_BUFFER_SIZE	
-			#define SONG_BUFFER_SIZE		24
+			#define SONG_BUFFER_SIZE	24
 		#endif
-		#ifndef SONG_BUFFER_MINIMUM
-			#define SONG_BUFFER_MINIMUM		SONG_BUFFER_SIZE/4
+		#ifndef SONG_BUFFER_MIN
+			#define SONG_BUFFER_MIN		SONG_BUFFER_SIZE/4
 		#endif
 	#else // MOD
 		extern void StartSong(const char *song, u16 startPos, bool loop);
